c81264498a090f7f7b78990bfe9f917f7dc72db0
shubhamd <d.shub07@gmail.com>
2021-03-31 20:49:23 +0530

    Spring security project

---
+++ SecureApp/src/main/java/com/SpringSecurity/secureApp/UserRepository.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.secureApp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	 
+	User findByUsername(String username);
+	
+	
+	
+}
